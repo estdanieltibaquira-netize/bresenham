95a94e1fe39ba8c887d0581267aca875d3116c51
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-04-05 20:52:46 -0500

    Implemento función dibujar para obtener valores ingresados por el usuario

---
+++ script.js
@@ -0,0 +1,9 @@
+function dibujar() {
+
+    let x0 = parseInt(document.getElementById("x0").value);
+    let y0 = parseInt(document.getElementById("y0").value);
+    let x1 = parseInt(document.getElementById("x1").value);
+    let y1 = parseInt(document.getElementById("y1").value);
+
+    console.log(x0, y0, x1, y1);
+}
